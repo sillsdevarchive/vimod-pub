92a0cc263f71e5192334434aab1a284756cff3e1
Ian McQuay <ian_mcquay@sil.org>
2013-09-12 15:53:18 +0800

    Major change. It get a parameter passed from PT that it writes to a batch file. The rest of the batch file is written by the script too. Then the batch file is called, that starts vimod-pub pub.exe.
    This format is extensible to do other things.

--- resources/ParatextIntergration/vimodpub.py
+++ resources/ParatextIntergration/vimodpub.py
@@ -4,8 +4,24 @@
 
 # Import OS to be able to output correctly to Windows 
 import os
-
-# Send a command to start the LD_Link program
-# all backslashes must be doubled as it is a Python escape character
-os.startfile("D:\\My Paratext Projects\\cms\\vimodpub.cmd")
-#os.startfile("C:\\My Paratext Projects\\cms\\vimodpub.cmd")
+batchfile = "C:\\Temp\\vimodpub-link.cmd"
+# write to batch file echo off. Overwrite previous file.
+commandstring = "echo @echo off >" + batchfile
+os.system(commandstring)
+# append to batch file the variable from PT cms option
+commandstring = "echo set vimodfolder=" + vimodfolder + ">>" + batchfile
+os.system(commandstring)
+# append to batch file get the drive letter
+commandstring = "echo set drive=%vimodfolder:~0,2%>>" + batchfile
+os.system(commandstring)
+# append to batch file swap to the correct drive
+commandstring = "echo call %drive%>>" + batchfile
+os.system(commandstring)
+# append to batch file change path to vimod-pub
+commandstring = "echo cd \"%vimodfolder%\">>" + batchfile
+os.system(commandstring)
+# append to batch file  the call to Vimod-Pub pub.exe
+commandstring = "echo call \"%vimodfolder%\pub.cmd\">>" + batchfile
+os.system(commandstring)
+# call the just written file
+os.startfile(batchfile)
