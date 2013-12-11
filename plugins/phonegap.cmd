274391769a5521773ae67ec7f42d99c9d5843d53
Ian McQuay <ian_mcquay@sil.org>
2013-12-11 12:32:26 +0800

    minor doc changes Force Phonegap to do local build.

--- plugins/phonegap.cmd
+++ plugins/phonegap.cmd
@@ -1,6 +1,26 @@
 :: Plugin Name: Phonegap
-:: Purpose: Intergrate Phonegap creat and build processes into VimodPub
+:: Purpose: Intergrate Phonegap create and build processes into VimodPub
 :control
+:: Description: Test if phonegap is installed on computer. Exits batch if not installed or runs pluginsubtask.
+:: Required preset variables:
+:: pluginsubtask
+:: params
+:: Required functions:
+:: secondline
+set phonegapinstalled=
+set testphonegap=logs\phonegap-usage.txt
+rem the following line will generate a file. If node and phonegap is installed the file will start with "Usage: node"
+node "%appdata%\npm\node_modules\phonegap\bin\phonegap.js" --help  >%testphonegap%
+call :getlineafter 1 "%testphonegap%"
+if "%line:~0,11%" == "Usage: node" set phonegapinstalled=yes
+if not defined phonegapinstalled (
+    echo Phonegap not installed, or at lease not found in the path.
+    echo Please read the following web page and install as instructed.
+    echo http://docs.phonegap.com/en/3.0.0/guide_cli_index.md.html
+    echo Phonegap will not run. This function will exit after you press any key.
+    pause
+    exit /b
+)
 call :%pluginsubtask% %params%
 goto :eof
 
@@ -26,7 +46,7 @@ goto :eof
 :: Required functions: 
 :: common
 
-set curcommand=call phonegap build android
+set curcommand=call phonegap local build android
 call :common %phonegapfolder%
 goto :eof
 
@@ -43,11 +63,8 @@ goto :eof
 
 :common
 :: Description: Runs the various options
-:: Required preset variables:
-:: Optional preset variables:
 :: Required parameters:
-:: Optional parameters:
-:: Required functions:
+:: phonegapfolder
 set startdir=%cd%
 call plugins\beforeplugincurcommand
 cd "%projectpath%\%~1"
@@ -55,4 +72,13 @@ cd "%projectpath%\%~1"
 %curcommand%
 @echo off
 cd "%startdir%"
+goto :eof
+
+
+
+:getlineafter
+for /f "skip=%~1 delims=" %%i in (%~2) do (
+    set line=%%i
+    goto :eof 
+)
 goto :eof
